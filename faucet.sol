b18f2759e1d9d2a2d0d50a5d6f482d5c0ef024d4
kr-urvish <123373193+kr-urvish@users.noreply.github.com>
2023-02-11 12:26:44 +0530

    Update faucet.sol
    final v5

--- faucet.sol
+++ faucet.sol
@@ -1,49 +1,58 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.0;
 
-//import "remix_tests.sol";
-// import "../contracts/MyToken.sol";
 import "MyToken.sol";
 
 contract Faucet{
 
 // creat obj
     MyToken fooContract;
-    address public urvish = 0x9f477448F1B96edCE91d0e29Aa0A1f4E816C9A5f;
-    address public owner;
+    address public urvish = 0x9f477448F1B96edCE91d0e29Aa0A1f4E816C9A5f; // tpken account address
+ 
+    //mapping to keep track of requested rokens
+    //Address and blocktime + 1 day is saved in TimeLock
+    mapping(address => uint256) public lockTime;
+    mapping(address => uint) public withdrawals_balance;
 
-
+    //constroctur 
     constructor(address fooAddress) {
         fooContract = MyToken(fooAddress);
-        owner = urvish;
     }
 
-    //address public new_owner = msg.sender;
-     address public xyz = address(this);
+    //withdraw function
+    function withdraw(address to, uint amt) public returns(bool){
 
+        //old uders
+        if(lockTime[to] != 0){
 
-    // function approveStakingPoolOnFoo(address faucetAdd, uint256 amount) public returns(bool) {
-    //     require(fooContract.approve(faucetAdd,amount),
-    //         "approveOnFoo:approval failed");
-    //     return true;
-    // }
+            //user come after 24H
+            if(lockTime[to] + 1 days <= block.timestamp){
+                withdrawals_balance[to] = 0;
+            }
+          
+            require(amt <= 100 - withdrawals_balance[to] , "you can clime only 100 coins in 24H");
+            require(fooContract.transferFrom(urvish,to, amt), "Deposit is fall");
 
+            withdrawals_balance[to] = amt;  // update balances
+            //update time only after 24H
+            if(lockTime[to] + 1 days <= block.timestamp){
+                lockTime[to] = block.timestamp;
+            }
 
-    mapping ( address => uint256 ) public balances;
-    
-    uint256 amt = 100*(10**18);
-
-    function deposit(address to) public returns(bool){
-
-        // add the deposited tokens into existing balance 
-       // balances[msg.sender]+= amt;
-
-        // transfer the tokens from the sender to this contract
-        require(fooContract.transferFrom(urvish,to, amt),
-                    "Deposit is fall");
-
+              
+        }
+        else{ 
+            // new users
+            require(amt <=100, "you can clime only 100 coins in 24H");
+            //deposit
+            require(fooContract.transferFrom(urvish,to, amt), "Deposit is fall");
+            //update balances
+            withdrawals_balance[to] = amt;
+            //update time
+            lockTime[to] = block.timestamp;
+        }
+        
         return true;
     }
-
 
 }
